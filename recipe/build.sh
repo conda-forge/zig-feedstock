e643f2fd6554ae30248bcfbcc73be610c6331f45
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-12-08 02:09:14 -0300

    use lld

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,23 +5,19 @@ set -ex
 mkdir -p build
 cd build
 
-if [[ "$(uname -s)" == "Linux" ]]; then
-  cmake .. ${CMAKE_ARGS} \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER=${CC} \
-    -DCMAKE_CXX_COMPILER=${CXX} \
-    -DZIG_PREFER_CLANG_CPP_DYLIB=yes
-else  # darwin
-  cmake .. ${CMAKE_ARGS} \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_PREFIX_PATH=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER=${CC} \
-    -DCMAKE_CXX_COMPILER=${CXX} \
-    -DZIG_STATIC_LLVM=ON
+if [[ "$(uname -s)" == "Darwin" ]]; then
+  EXTRA_CMAKE_ARGS="$EXTRA_CMAKE_ARGS -DZIG_STATIC_LLVM=ON"
 fi
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_C_COMPILER=${CC} \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_CXX_FLAGS="-fuse-ld=lld" \
+  ${EXTRA_CMAKE_ARGS} \
+  ..
 
 cmake --build .
 cmake --install . -v
