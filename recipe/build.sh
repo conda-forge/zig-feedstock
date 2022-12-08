e28d5c39b7824863e635b3aa1dfccdb5cf0483ba
Guilherme Leobas <guilhermeleobas@gmail.com>
2022-12-08 01:32:40 -0300

    zig 0.10.0 and osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,13 +5,23 @@ set -ex
 mkdir -p build
 cd build
 
-cmake .. ${CMAKE_ARGS} \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_C_COMPILER=${CC} \
-  -DCMAKE_CXX_COMPILER=${CXX} \
-  -DZIG_PREFER_CLANG_CPP_DYLIB=yes
+if [[ "$(uname -s)" == "Linux" ]]; then
+  cmake .. ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DZIG_PREFER_CLANG_CPP_DYLIB=yes
+else  # darwin
+  cmake .. ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_C_COMPILER=${CC} \
+    -DCMAKE_CXX_COMPILER=${CXX} \
+    -DZIG_STATIC_LLVM=ON
+fi
 
 cmake --build .
 cmake --install . -v
