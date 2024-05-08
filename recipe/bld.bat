f468841561c1a1bf4b2ea8773b9c77f57725c663
memento <claude.rc@gmail.com>
2024-05-08 09:39:20 -0500

    (fix) try zig build itself on windows. update clang on mac

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,17 @@
 @echo off
-powershell -File "%RECIPE_DIR%\x86_64-windows.ps1"
+:: powershell -File "%RECIPE_DIR%\x86_64-windows.ps1"
+
+:: Try using bootstrapped ZIG to build ZIG for Conda environment
+set "TARGET=x86_64-windows-gnu"
+set "MCPU=native"
+set "ZIG = "%SRC_DIR%\zig-bootstrap\zig.exe"
+
+%ZIG% build ^
+  --prefix "%PREFIX%" ^
+  --search-prefix "%PREFIX%" ^
+  -Dflat ^
+  -Doptimize=ReleaseFast ^
+  -Dstrip ^
+  -Dtarget="%TARGET%" ^
+  -Dcpu="%MCPU%"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
--- recipe/x86_64-windows.ps1
+++ recipe/x86_64-windows.ps1
@@ -1,8 +1,9 @@
 $TARGET = "x86_64-windows-gnu"
-$ZIG_LLVM_CLANG_LLD_NAME = "zig+llvm+lld+clang-$TARGET-0.12.0-dev.2073+402fe565a"
 $MCPU = "baseline"
+HOST_TARGET="x86_64-windows-msvc"
 $CONDA_PREFIX = "$PREFIX"
-$PREFIX_PATH = "$($Env:USERPROFILE)\$ZIG_LLVM_CLANG_LLD_NAME"
+
+
 $ZIG = "$($Env:SRC_DIR)\zig-bootstrap\zig.exe"
 $ZIG_LIB_DIR = "$(Get-Location)\lib"
 
@@ -23,7 +24,7 @@ Set-Location -Path 'build-release'
 & cmake .. `
   -GNinja `
   -DCMAKE_INSTALL_PREFIX="$($CONDA_PREFIX -Replace "\\", "/")" `
-  -DCMAKE_PREFIX_PATH="$($PREFIX_PATH -Replace "\\", "/")" `
+  -DCMAKE_PREFIX_PATH="$($CONDA_PREFIX -Replace "\\", "/")" `
   -DCMAKE_BUILD_TYPE=Release `
   -DCMAKE_C_COMPILER="$($ZIG -Replace "\\", "/");cc;-target;$TARGET;-mcpu=$MCPU" `
   -DCMAKE_CXX_COMPILER="$($ZIG -Replace "\\", "/");c++;-target;$TARGET;-mcpu=$MCPU" `
@@ -34,4 +35,51 @@ Set-Location -Path 'build-release'
 CheckLastExitCode
 
 ninja install
+CheckLastExitCode
+
+Write-Output "Main test suite..."
+& "stage3-release\bin\zig.exe" build test docs `
+  --zig-lib-dir "$ZIG_LIB_DIR" `
+  --search-prefix "$PREFIX_PATH" `
+  -Dstatic-llvm `
+  -Dskip-non-native `
+  -Denable-symlinks-windows
+CheckLastExitCode
+
+Write-Output "Build x86_64-windows-msvc behavior tests using the C backend..."
+& "stage3-release\bin\zig.exe" test `
+  ..\test\behavior.zig `
+  --zig-lib-dir "$ZIG_LIB_DIR" `
+  -ofmt=c `
+  -femit-bin="test-x86_64-windows-msvc.c" `
+  --test-no-exec `
+  -target x86_64-windows-msvc `
+  -lc
+CheckLastExitCode
+
+& "stage3-release\bin\zig.exe" build-obj `
+  --zig-lib-dir "$ZIG_LIB_DIR" `
+  -ofmt=c `
+  -OReleaseSmall `
+  --name compiler_rt `
+  -femit-bin="compiler_rt-x86_64-windows-msvc.c" `
+  --dep build_options `
+  -target x86_64-windows-msvc `
+  --mod root ..\lib\compiler_rt.zig `
+  --mod build_options config.zig
+CheckLastExitCode
+
+Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+CheckLastExitCode
+
+Enter-VsDevShell -VsInstallPath "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools" `
+  -DevCmdArguments '-arch=x64 -no_logo' `
+  -StartInPath $(Get-Location)
+CheckLastExitCode
+
+Write-Output "Build and run behavior tests with msvc..."
+& cl.exe -I..\lib test-x86_64-windows-msvc.c compiler_rt-x86_64-windows-msvc.c /W3 /Z7 -link -nologo -debug -subsystem:console kernel32.lib ntdll.lib libcmt.lib
+CheckLastExitCode
+
+& .\test-x86_64-windows-msvc.exe
 CheckLastExitCode
