8139135d767d9084ed8cca698f95695fa3b527a6
memento <claude.rc@gmail.com>
2024-05-08 14:01:18 -0500

    (fix) windows msvc. mac selector for bootstrap

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@
 :: powershell -File "%RECIPE_DIR%\x86_64-windows.ps1"
 
 :: Try using bootstrapped ZIG to build ZIG for Conda environment
-set "TARGET=x86_64-windows-gnu"
+set "TARGET=x86_64-windows-msvc"
 set "MCPU=native"
 set "ZIG=%SRC_DIR%\zig-bootstrap\zig.exe"
 
