321559ed2b2ea8acb6c350340b6e153393d5643e
memento <claude.rc@gmail.com>
2024-05-13 15:35:12 -0500

    (fix) mac test fails libcxx? which win dir not found. linux no luck

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,23 +13,33 @@ set "CONFIG_DIR=%SRC_DIR%\_config"
 set "ZIG_BUILD_DIR=%SRC_DIR%\_build"
 set "ZIG_TEST_DIR=%SRC_DIR%\_self-build"
 
+echo "Configuring ZIG in %CONFIG_DIR% from %SOURCE_DIR%"
 mkdir %CONFIG_DIR%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %CONFIG_DIR%
-cmake %SOURCE_DIR% ^
-  -G "Ninja" ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
-  -DCMAKE_PREFIX_PATH="%PREFIX%" ^
-  -DZIG_TARGET_TRIPLE="%HOST_TARGET%" ^
-  -DZIG_TARGET_MCPU=baseline ^
-  -DZIG_VERSION="%PKG_VERSION%"
+  cmake %SOURCE_DIR% ^
+    -G "Ninja" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%PREFIX%" ^
+    -DZIG_TARGET_TRIPLE="%HOST_TARGET%" ^
+    -DZIG_TARGET_MCPU=baseline ^
+    -DZIG_VERSION="%PKG_VERSION%"
+  if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %SRC_DIR%
 
+echo "Building ZIG with: %ZIG% in %ZIG_BUILD_DIR%"
 mkdir %ZIG_BUILD_DIR%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %ZIG_BUILD_DIR%
+  echo "   Copying sources ..."
+  dir %SOURCE_DIR%
   xcopy /E %SOURCE_DIR%\* . > nul
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+  echo "   Building ..."
+  dir %CONFIG_DIR%
+  dir %CONFIG_DIR%/config.h
   %ZIG% build ^
     --prefix "%PREFIX%" ^
     -Dconfig_h="%CONFIG_DIR%/config.h" ^
@@ -41,14 +51,19 @@ cd %ZIG_BUILD_DIR%
   ::  -Dtarget="%TARGET%" ^
   ::  -Dcpu="%MCPU%" ^
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-cd ..
+cd %SRC_DIR%
 
+echo "Building ZIG with: %PREFIX%\bin\zig.exe in %ZIG_TEST_DIR%"
 mkdir %ZIG_TEST_DIR%
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 cd %ZIG_TEST_DIR%
+  echo "   Copying sources ..."
   xcopy /E %SOURCE_DIR%\* . > nul
-  set "ZIG=%PREFIX%\bin\zig.exe"
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
+  set "ZIG=%PREFIX%\bin\zig.exe"
+
+  echo "   Building ..."
   %ZIG% build ^
     --prefix "%SRC_DIR%/_self-test" ^
     -Dconfig_h="%CONFIG_DIR%/config.h" ^
@@ -57,4 +72,4 @@ cd %ZIG_TEST_DIR%
     -Dstrip ^
     -Dversion-string="%PKG_VERSION%"
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-cd ..
+cd %SRC_DIR%
