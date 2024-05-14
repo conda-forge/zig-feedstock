dc9d71106f87d72a1bfec0a9438386c9a993a66a
memento <claude.rc@gmail.com>
2024-05-14 10:31:48 -0500

    DBG: Too confused, verifying linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,8 +14,8 @@ function configure_linux_64() {
     TARGET="x86_64-linux-gnu"
     MCPU="baseline"
 
-    echo "${CMAKE_ARGS}"
     cmake "${SRC_DIR}"/zig-source \
+      "${CMAKE_ARGS}" \
       -D CMAKE_INSTALL_PREFIX="${install_dir}" \
       -D CMAKE_PREFIX_PATH="${PREFIX};${PREFIX}/lib;${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64;${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/usr/lib64" \
       -D CMAKE_BUILD_TYPE=Release \
@@ -28,7 +28,6 @@ function configure_linux_64() {
       -D ZIG_USE_LLVM_CONFIG=ON \
       -D ZIG_TARGET_DYNAMIC_LINKER="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-${LIBC_CONDA_VERSION-2.28}.so" \
       -G Ninja
-      # "${CMAKE_ARGS}" \
   cd "${current_dir}"
 }
 
@@ -131,7 +130,7 @@ case "$(uname)" in
     cmake_build_install "${SRC_DIR}/build-release"
     patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --remove-rpath "${PREFIX}/bin/zig"
     export LD_LIBRARY_PATH="${PREFIX}/lib"
-    self_build_x86_64 "${SRC_DIR}/self-built-source" "${PREFIX}" "${SRC_DIR}/_self-built"
+    # self_build_x86_64 "${SRC_DIR}/self-built-source" "${PREFIX}" "${SRC_DIR}/_self-built"
     ;;
   Darwin)
     ZIG="${SRC_DIR}/zig-bootstrap/zig"
