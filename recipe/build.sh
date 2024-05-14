d3937e5106e6df418707841e922c37f67842dc9f
memento <claude.rc@gmail.com>
2024-05-14 15:02:53 -0500

    (ref) more cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,11 +25,7 @@ function configure_linux_64() {
       -D ZIG_USE_LLVM_CONFIG=ON \
       -D ZIG_TARGET_DYNAMIC_LINKER="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-${LIBC_CONDA_VERSION-2.28}.so" \
       -G Ninja
-      # -D LIBC_CONDA_VERSION="${LIBC_CONDA_VERSION-2.28}" \
-      # -D LIBC_INTERPRETER="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64/ld-${LIBC_CONDA_VERSION-2.28}.so" \
-      # -D LIBC_RPATH="${BUILD_PREFIX}/x86_64-conda-linux-gnu/sysroot/lib64:${BUILD_PREFIX}/x86_64-conda-linux-gnu/lib:${PREFIX}/lib64:${PREFIX}/lib" \
       # "${CMAKE_ARGS}" \
-      # -D CMAKE_PROGRAM_PATH="${PREFIX}" \
   cd "${current_dir}"
 }
 
