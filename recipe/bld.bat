27bf145428e66c296cb2cdb438ee11edd76c6599
memento <claude.rc@gmail.com>
2024-05-15 16:28:02 -0500

    (dbg) lost, start at the beginning

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -14,34 +14,34 @@ set "ZIG_BUILD_DIR=%SRC_DIR%\_build"
 set "ZIG_INSTALL_DIR=%SRC_DIR%\_installed"
 set "ZIG_TEST_DIR=%SRC_DIR%\_self-build"
 
-echo "Configuring ZIG in %CONFIG_DIR% from %SOURCE_DIR%"
-mkdir %CONFIG_DIR%
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-cd %CONFIG_DIR%
-  set "PATH=%PREFIX%\bin;%PATH%"
-  cmake %SOURCE_DIR% ^
-    -G "Ninja" ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_INSTALL_PREFIX="%PREFIX%" ^
-    -D CMAKE_PREFIX_PATH="%PREFIX%" ^
-    -D ZIG_TARGET_TRIPLE="%TARGET%" ^
-    -D ZIG_TARGET_MCPU=baseline ^
-    -D ZIG_SHARED_LLVM=ON ^
-    -D ZIG_VERSION="%PKG_VERSION%"
-  if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-    :: Shared libs are seemingly not supported on Windows MSVC (maybe switch to mingw?)
-    :: -D ZIG_SHARED_LLVM=ON ^
-    :: -D ZIG_USE_LLVM_CONFIG=ON ^
-cd %SRC_DIR%
+:: echo "Configuring ZIG in %CONFIG_DIR% from %SOURCE_DIR%"
+:: mkdir %CONFIG_DIR%
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+:: cd %CONFIG_DIR%
+::   set "PATH=%PREFIX%\bin;%PATH%"
+::   cmake %SOURCE_DIR% ^
+::     -G "Ninja" ^
+::     -D CMAKE_BUILD_TYPE=Release ^
+::     -D CMAKE_INSTALL_PREFIX="%PREFIX%" ^
+::     -D CMAKE_PREFIX_PATH="%PREFIX%" ^
+::     -D ZIG_TARGET_TRIPLE="%TARGET%" ^
+::     -D ZIG_TARGET_MCPU=baseline ^
+::     -D ZIG_SHARED_LLVM=ON ^
+::     -D ZIG_VERSION="%PKG_VERSION%"
+::   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+::
+::     :: Shared libs are seemingly not supported on Windows MSVC (maybe switch to mingw?)
+::     :: -D ZIG_SHARED_LLVM=ON ^
+::     :: -D ZIG_USE_LLVM_CONFIG=ON ^
+:: cd %SRC_DIR%
 
 :: echo "Building ZIG from source in %CONFIG_DIR%
-cd %CONFIG_DIR%
-  echo "   Building ..."
-  cmake --build . --config Release --target install -- -j %NUMBER_OF_PROCESSORS%
-  if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-  echo "   Built."
-cd %SRC_DIR%
+:: cd %CONFIG_DIR%
+::   echo "   Building ..."
+::   cmake --build . --config Release --target install -- -j %NUMBER_OF_PROCESSORS%
+::   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+::   echo "   Built."
+:: cd %SRC_DIR%
 
 echo "Building ZIG with: %ZIG% in %ZIG_BUILD_DIR%"
 mkdir %ZIG_BUILD_DIR%
@@ -56,9 +56,10 @@ cd %ZIG_BUILD_DIR%
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
   %ZIG% build ^
     --prefix "%ZIG_INSTALL_DIR%" ^
-    -Dconfig_h="%CONFIG_DIR%/config.h" ^
-    -Denable-llvm ^
+    --search-prefix "%BUILD_PREFIX%" ^
+    --search-prefix "%PREFIX%" ^
     -Doptimize=ReleaseFast ^
+    -Dstrip ^
     -Dtarget="%HOST_TARGET%" ^
     -Dcpu="%MCPU%" ^
     -Dversion-string="%PKG_VERSION%"
@@ -81,7 +82,6 @@ cd %ZIG_TEST_DIR%
   mkdir %SRC_DIR%\_self-test
   %ZIG% build ^
     --prefix "%SRC_DIR%/_self-test" ^
-    -Dconfig_h="%CONFIG_DIR%/config.h" ^
     -Dflat ^
     -Doptimize=ReleaseFast ^
     -Dstrip ^
