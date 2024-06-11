e2826828a7a9338556833a6fa10829b4c5aef616
memento <claude.rc@gmail.com>
2024-06-10 21:08:17 -0500

    (dbg) .

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -52,14 +52,14 @@ cd %CONFIG_DIR%
     -D CMAKE_BUILD_TYPE=Release ^
     -D CMAKE_INSTALL_PREFIX="%_zig_install_dir%" ^
     -D CMAKE_PREFIX_PATH="%_prefix%\\Library\\lib" ^
+    -D CMAKE_C_COMPILER="%_zig%;cc" ^
+    -D CMAKE_CXX_COMPILER="%_zig%;c++" ^
+    -D CMAKE_AR="%_zig%" ^
     -D ZIG_AR_WORKAROUND=ON ^
     -D ZIG_USE_LLVM_CONFIG=OFF ^
     -D ZIG_SHARED_LLVM=ON ^
     -D ZIG_VERSION="%PKG_VERSION%"
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-    :: -D CMAKE_C_COMPILER="%_zig%;cc" ^
-    :: -D CMAKE_CXX_COMPILER="%_zig%;c++" ^
-    :: -D CMAKE_AR="%_zig%" ^
     :: -D ZIG_SYSTEM_LIBCXX="c++" ^
 cd %SRC_DIR%
 GOTO :EOF
