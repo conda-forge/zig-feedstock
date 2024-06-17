2beaa723f472f349f2ecf545ca209f237b65358f
memento <claude.rc@gmail.com>
2024-06-17 18:48:59 -0500

    (dbg) .

--- recipe/build.sh
+++ recipe/build.sh
@@ -102,6 +102,7 @@ function self_build() {
     echo "     ${config_h}"
     ls -l "${zig}"
     uname -a
+    $zig build.exe -mcpu=ppc64le
     $zig build -Dcpu=ppc64
 
     mkdir -p "${install_dir}"
