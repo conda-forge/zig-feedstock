69562e6bb1a2adf0170b09615aba389364e37b5e
memento <claude.rc@gmail.com>
2024-06-18 18:47:10 -0500

    (dbg)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -60,7 +60,7 @@ cd %_build_dir%
   set "CLANG_MAXIMUM_CONCURRENT_JOBS=1"
   cmake %SOURCE_DIR% ^
     -G "Ninja" ^
-    -D CMAKE_BUILD_TYPE=Release ^
+    -D CMAKE_BUILD_TYPE=Debug ^
     -D CMAKE_INSTALL_PREFIX="%_zig_install_dir%" ^
     -D ZIG_USE_LLVM_CONFIG=ON ^
     -D ZIG_SHARED_LLVM=ON ^
