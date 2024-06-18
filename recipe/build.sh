8f77a97f4a94d69594622cf8eea06b11533f7b3d
memento <claude.rc@gmail.com>
2024-06-18 14:20:59 -0500

    (dbg) .

--- recipe/build.sh
+++ recipe/build.sh
@@ -137,8 +137,6 @@ if [[ "${target_platform}" == "linux-ppc64le" ]]; then
   cat /proc/cpuinfo
   configure_cmake "${cmake_build_dir}" "${cmake_install_dir}"
   cd "${cmake_build_dir}" && cmake --build . --target zig2.c -- -j"${CPU_COUNT}"
-  grep -3 "Sigaction__3652" "${cmake_build_dir}/zig2.c"
-  grep -3 "signal.h" "${cmake_build_dir}/zig2.c"
   echo "------------------"
   cmake_build_install "${cmake_build_dir}"
 fi
