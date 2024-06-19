1954749f9e448e41e3dd63f4291461cef1c522d0
memento <claude.rc@gmail.com>
2024-06-19 11:55:55 -0500

    (dbg)

--- recipe/build.sh
+++ recipe/build.sh
@@ -127,6 +127,11 @@ if [[ "${target_platform}" == "linux-ppc64le" ]]; then
   EXTRA_CMAKE_ARGS+=("-DZIG_TARGET_TRIPLE=${TARGET}")
   EXTRA_CMAKE_ARGS+=("-DZIG_TARGET_MCPU=${MCPU}")
   cat /proc/cpuinfo
+  (cd "${BUILD_PREFIX}"/${SYSROOT_ARCH}-conda-linux-gnu/sysroot && \
+    expand -t 4 -d usr/include/signal.h > usr/include/signal.h.tmp && \
+    mv usr/include/signal.h.tmp usr/include/signal.h && \
+    patch -Np0 -i "${RECIPE_DIR}"/patches/xxxx-ppc64le-sysroot.patch --binary \
+  )
   configure_cmake "${cmake_build_dir}" "${cmake_install_dir}"
   cd "${cmake_build_dir}" && cmake --build . --target zig2.c -- -j"${CPU_COUNT}"
   echo "------------------"
