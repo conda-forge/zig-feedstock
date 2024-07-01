4ca63b93768e247b5a2ea5f12dfa10017766c7b8
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-07-01 23:00:16 +0200

    ..

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 mkdir build
 cd build
 
-cmake $CMAKE_ARGS -GNinja $SRC_DIR/zig-source
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    CMAKE_ARGS="$CMAKE_ARGS -DZIG_USE_LLVM_CONFIG=OFF"
+fi
 
+cmake -GNinja $CMAKE_ARGS $SRC_DIR/zig-source
+     
 ninja install
