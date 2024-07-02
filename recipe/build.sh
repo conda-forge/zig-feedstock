143c90a7e1bc04ba649308a87517c6449f2a09d4
memento <claude.rc@gmail.com>
2024-07-01 21:49:47 -0500

    (fix) add libxml2.a

--- recipe/build.sh
+++ recipe/build.sh
@@ -139,6 +139,8 @@ function configure_cmake_zigcpp() {
 
     if [[ "${target_platform}" == "osx-64" ]]; then
       sed -i '' "s@;-lm@;$PREFIX/lib/libc++.dylib;-lm@" "${cmake_build_dir}/config.h"
+    elif [[ "${target_platform}" == "osx-arm64" ]]; then
+      sed -i "s@libLLVMXRay.a@libLLVMXRay.a;$PREFIX/lib/libxml2.a@" "${cmake_build_dir}/config.h"
     fi
 
     cmake --build . --target zigcpp -- -j"${CPU_COUNT}"
