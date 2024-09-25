888d55741e5c5f1179dd8c4f4f75d74b1a1bd581
memento <claude.rc@gmail.com>
2024-09-25 10:54:58 -0500

    (dbg) set interpreter

--- recipe/build_scripts/cross-zig-linux-aarch64.sh
+++ recipe/build_scripts/cross-zig-linux-aarch64.sh
@@ -23,10 +23,10 @@ SYSROOT_ARCH="aarch64"
 zig="${BUILD_PREFIX}/bin/zig"
 
 _BUILD_SYSROOT_ARCH="x86_64"
+patchelf --set-interpreter "${BUILD_PREFIX}/${_BUILD_SYSROOT_ARCH}-conda-linux-gnu/sysroot/lib64/ld-2.28.so" "${BUILD_PREFIX}/bin/zig"
 patchelf --set-rpath "${BUILD_PREFIX}/${_BUILD_SYSROOT_ARCH}-conda-linux-gnu/sysroot/lib64" "${BUILD_PREFIX}/bin/zig"
 patchelf --add-rpath "${BUILD_PREFIX}/${_BUILD_SYSROOT_ARCH}-conda-linux-gnu/sysroot/usr//lib64" "${BUILD_PREFIX}/bin/zig"
 patchelf --add-rpath "${BUILD_PREFIX}/lib" "${BUILD_PREFIX}/bin/zig"
-# patchelf --set-interpreter "${BUILD_PREFIX}/${_BUILD_SYSROOT_ARCH}-conda-linux-gnu/sysroot/lib64/ld-2.28.so" "${BUILD_PREFIX}/bin/zig"
 
 ${zig} --version
 
