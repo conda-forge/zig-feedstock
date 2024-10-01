a13a910b9f717da175eaa7c560ed1cb69965ca02
memento <claude.rc@gmail.com>
2024-10-01 17:45:42 -0500

    (dbg) no way out

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -15,8 +15,8 @@ mkdir -p "${cmake_build_dir}" && cp -r "${SRC_DIR}"/zig-source/* "${cmake_build_
 
 # Current conda zig may not be able to build the latest zig
 SYSROOT_ARCH="x86_64"
-
-where /R ${PREFIX} libmsvcrt.a
+PREFIX_WIN=$(echo "$PREFIX" | sed 's/\//\\/g')
+where /R "${PREFIX_WIN}" libmsvcrt.a
 
 _LIBPATH="${LIBPATH//\\//}"
 EXTRA_CMAKE_ARGS+=( \
