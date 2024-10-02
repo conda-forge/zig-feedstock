940996df846421365f868818f288a2403286d247
memento <claude.rc@gmail.com>
2024-10-02 11:54:17 -0500

    (dbg) no way out

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -16,6 +16,8 @@ mkdir -p "${cmake_build_dir}" && cp -r "${SRC_DIR}"/zig-source/* "${cmake_build_
 # Current conda zig may not be able to build the latest zig
 SYSROOT_ARCH="x86_64"
 
+dir "$BUILD_PREFIX"/Library/mingw-w64/lib
+
 _LIBPATH="${LIBPATH//\\//}"
 EXTRA_CMAKE_ARGS+=( \
   "-DCMAKE_BUILD_TYPE=Release" \
