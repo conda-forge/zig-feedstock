5f3547780e6d49466bb6bfa28ed0ace0c4bbff51
memento <claude.rc@gmail.com>
2024-10-10 20:07:48 -0500

    (dbg) msvc as target

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -16,13 +16,13 @@ mkdir -p "${cmake_build_dir}" && cp -r "${SRC_DIR}"/zig-source/* "${cmake_build_
 # Current conda zig may not be able to build the latest zig
 SYSROOT_ARCH="x86_64"
 
-_UCRT_LIBPATH="C:\Program Files (x86)/Windows Kits\10\lib\10.0.22621.0\ucrt\x64"
+_UCRT_LIBPATH="C:\Program Files (x86)\Windows Kits\10\lib\10.0.22621.0\ucrt\x64"
 dir "${_UCRT_LIBPATH}"
 
 EXTRA_CMAKE_ARGS+=( \
   "-DCMAKE_BUILD_TYPE=Release" \
   "-DCMAKE_VERBOSE_MAKEFILE=ON" \
-  "-DZIG_CMAKE_PREFIX_PATH=${_UCRT_LIBPATH//\\//};${LIBPATH//\\//}" \
+  "-DZIG_CMAKE_PREFIX_PATH=${_UCRT_LIBPATH};${LIBPATH//\\//}" \
   "-DZIG_TARGET_TRIPLE=${SYSROOT_ARCH}-windows-msvc" \
 )
   # "-DZIG_SYSTEM_LIBCXX='c++'" \
