b59ad56f61c21ce8aa242935874f9bfb4c9a4456
memento <claude.rc@gmail.com>
2024-10-20 15:04:17 -0500

    (dbg) mkdir not found ?

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -31,7 +31,7 @@ EXTRA_CMAKE_ARGS+=( \
 # When using installed c++ libs, zig needs libzigcpp.a
 configure_cmake_zigcpp "${cmake_build_dir}" "${PREFIX}"
 
-sed -i '' 's#ZIG_LLVM_LIB_PATH "#ZIG_LLVM_LIB_PATH "C:/Windows/System32#' "${cmake_build_dir}"/config.h
+sed -i 's#ZIG_LLVM_LIB_PATH "#ZIG_LLVM_LIB_PATH "C:/Windows/System32#' "${cmake_build_dir}"/config.h
 
 pushd "${cmake_build_dir}"
   cat config.h || true
