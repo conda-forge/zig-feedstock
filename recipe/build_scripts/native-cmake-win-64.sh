b1f984b661db175c0454b314552ef7abae45f445
memento <claude.rc@gmail.com>
2024-10-22 15:52:12 -0500

    (dbg) mkdir not found ?

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -34,6 +34,7 @@ configure_cmake "${cmake_build_dir}" "${PREFIX}"
 pushd "${cmake_build_dir}"
   # sed -i 's#ZIG_LLVM_LIB_PATH "#ZIG_LLVM_LIB_PATH "C:/Windows/System32;C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;"#' config.h
   powershell -Command "(Get-Content config.h) -replace 'ZIG_LLVM_LIB_PATH \"', 'ZIG_LLVM_LIB_PATH \"C:/Windows/System32;C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;\"' | Set-Content config.h"
+  cat config.h
 popd
 
 cmake_build_install "${cmake_build_dir}"
