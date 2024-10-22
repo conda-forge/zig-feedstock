3734b9a188193747a08bfa98d8166501ca59f928
memento <claude.rc@gmail.com>
2024-10-22 15:51:53 -0500

    (dbg) mkdir not found ?

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -32,8 +32,8 @@ EXTRA_CMAKE_ARGS+=( \
 configure_cmake "${cmake_build_dir}" "${PREFIX}"
 
 pushd "${cmake_build_dir}"
-  sed -i 's#ZIG_LLVM_LIB_PATH ".*#ZIG_LLVM_LIB_PATH "C:/Windows/System32"\n#define ZIG_SYSTEM_LIB_PATH "C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64"#' config.h
-  # powershell -Command "(Get-Content config.h) -replace 'ZIG_LLVM_LIB_PATH \".+', \"#define ZIG_LLVM_LIB_PATH \\\"C:/Windows/System32\\\"$([Environment]::NewLine)#define ZIG_SYSTEM_LIB_PATH \\\"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64\\\"\" | Set-Content config.h"cat config.h || true
+  # sed -i 's#ZIG_LLVM_LIB_PATH "#ZIG_LLVM_LIB_PATH "C:/Windows/System32;C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;"#' config.h
+  powershell -Command "(Get-Content config.h) -replace 'ZIG_LLVM_LIB_PATH \"', 'ZIG_LLVM_LIB_PATH \"C:/Windows/System32;C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;\"' | Set-Content config.h"
 popd
 
 cmake_build_install "${cmake_build_dir}"
