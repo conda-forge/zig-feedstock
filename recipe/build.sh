93183f71692fd5e8ad44d5c450bb08d72a9424d0
memento <claude.rc@gmail.com>
2025-03-09 16:11:38 -0500

    (fix) only the ugliest for last

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,24 +9,24 @@ else
 fi
 
 case "${target_platform}" in
-  linux-64)
-    bash "${RECIPE_DIR}"/build_scripts/native-"${builder}"-linux-64.sh
+  linux-64,osx-64,win-64)
+    bash "${RECIPE_DIR}"/build_scripts/native-"${builder}-${target_platform}".sh
     ;;
-  osx-64)
-    bash "${RECIPE_DIR}"/build_scripts/native-"${builder}"-osx-64.sh
+  # osx-64)
+  #   bash "${RECIPE_DIR}"/build_scripts/native-"${builder}"-osx-64.sh
+  #   ;;
+  # win-64)
+  #   bash "${RECIPE_DIR}"/build_scripts/native-"${builder}"-win-64.sh
+  #   ;;
+  linux-aarch64,linux-ppc64le,osx-arm64)
+    bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}-${target_platform}".sh
     ;;
-  win-64)
-    bash "${RECIPE_DIR}"/build_scripts/native-"${builder}"-win-64.sh
-    ;;
-  linux-aarch64)
-    bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}"-linux-aarch64.sh
-    ;;
-  linux-ppc64le)
-    bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}"-linux-ppc64le.sh
-    ;;
-  osx-arm64)
-    bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}"-osx-arm64.sh
-    ;;
+  # linux-ppc64le)
+  #   bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}"-linux-ppc64le.sh
+  #   ;;
+  # osx-arm64)
+  #   bash "${RECIPE_DIR}"/build_scripts/cross-"${builder}"-osx-arm64.sh
+  #   ;;
   *)
     echo "Unsupported target_platform: ${target_platform}"
     exit 1
