77c0192882a9b0f3254cb43b53cabd083239bac7
memento <claude.rc@gmail.com>
2025-03-11 18:17:13 -0500

    (fix) upstream binary

--- recipe/build_scripts/native-zig-win-64.sh
+++ recipe/build_scripts/native-zig-win-64.sh
@@ -20,9 +20,10 @@ SYSROOT_ARCH="x86_64"
 
 _UCRT_LIBPATH="C:\Program Files (x86)\Windows Kits\10\lib\10.0.22621.0\um\x64;C:\Program Files (x86)\Windows Kits\10\lib\10.0.22621.0\ucrt\x64;C:\Windows\System32"
 
-${PREFIX}/bin/mamba create -yp conda_zig_env zig
-zig="${SRC_DIR}"/conda_zig_env/bin/zig
-export PATH="${SRC_DIR}/conda_zig_env/bin:${PATH}"
+# ${PREFIX}/bin/mamba create -yp conda_zig_env zig
+# zig="${SRC_DIR}"/conda_zig_env/bin/zig
+# export PATH="${SRC_DIR}/conda_zig_env/bin:${PATH}"
+zig="${SRC_DIR}"/zig-bootstrap/zig.exe
 
 EXTRA_CMAKE_ARGS+=( \
   "-DZIG_SHARED_LLVM=ON" \
