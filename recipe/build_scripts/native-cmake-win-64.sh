5f8ea7e8906189c65a83c39a60abf1359c8f50a6
memento <claude.rc@gmail.com>
2025-04-03 20:27:28 -0500

    (dbg) msvcrt

--- recipe/build_scripts/native-cmake-win-64.sh
+++ recipe/build_scripts/native-cmake-win-64.sh
@@ -25,7 +25,7 @@ _UCRT_LIBPATH="C:\Program Files (x86)\Windows Kits\10\lib\10.0.22621.0\um\x64;C:
 # Construct the path to msvcrt.lib using VSINSTALLDIR
 _MSVC_LIB_PATH="${VSINSTALLDIR//\\/\/}/VC/Tools/MSVC/$(get_msvc_version)/lib"
 
-echo "VSINSTALLDIR: ${VSINSTALLDIR}"
+ls "${_MSVC_LIB_PATH}"
 
 EXTRA_CMAKE_ARGS+=(
   "-DCMAKE_BUILD_TYPE=Release"
@@ -44,7 +44,7 @@ configure_cmake "${cmake_build_dir}" "${PREFIX}"
 pushd "${cmake_build_dir}"
   # This is very hack-ish, but it seemed impossible to tell stage3/zig to find the needed version, uuid, ole32, etc DLLs
   # It goes with a patch of build.zig to accept multiple paths
-  powershell -Command "(Get-Content config.h) -replace 'ZIG_LLVM_LIB_PATH \"', 'ZIG_LLVM_LIB_PATH \"C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;\"' | Set-Content config.h"
+  powershell -Command "(Get-Content config.h) -replace 'ZIG_LLVM_LIB_PATH \"', 'ZIG_LLVM_LIB_PATH \"${_MSVC_LIB_PATH};C:/Program Files (x86)/Windows Kits/10/Lib/10.0.22621.0/um/x64;\"' | Set-Content config.h"
   cat config.h
 popd
 
