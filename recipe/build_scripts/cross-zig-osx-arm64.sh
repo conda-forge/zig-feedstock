c12c02be68e9e9a1073435edf41dead1dd99a8ae
Tim de Jager <tdejager89@gmail.com>
2025-11-04 19:30:39 +0100

    fix: fix up some things that did not make sense

--- recipe/build_scripts/cross-zig-osx-arm64.sh
+++ recipe/build_scripts/cross-zig-osx-arm64.sh
@@ -20,8 +20,9 @@ cp -r "${RECIPE_DIR}"/patches/xxxx* "${SRC_DIR}"/build-level-patches
 
 SYSROOT_ARCH="aarch64"
 
-mamba create -n zig_env -y zig
-zig=$(mamba run -n zig_env which zig | grep -Eo '/.*zig' | tail -n 1)
+# mamba create -n zig_env -y zig
+# zig=$(mamba run -n zig_env which zig | grep -Eo '/.*zig' | tail -n 1)
+zig=zig
 
 # This is safe-keep for when non-backward compatible updates are introduced
 # zig="${SRC_DIR}/zig-bootstrap/zig"
